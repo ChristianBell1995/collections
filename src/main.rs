124a2be4f3cef77ce2abe25572e70a305237d225
Christian Bell <christiantcbell@gmail.com>
2019-07-21 16:49:48 +0100

    added mode function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     let mut med_vec = vec![45, 43, 88, 102, 55, 1, 2];
     let median = median(&mut med_vec);
     println!("The median is: {}", median);
+
+    let mode_vec = vec![45, 43, 88, 102, 102, 102, 55, 1, 2, 1];
+    let mode = mode(&mode_vec);
+    println!("The mode is: {}", mode);
 }
 
 // Given a list of integers, use a vector and return the mean (the average value),
@@ -29,4 +33,22 @@ fn median(vector: &mut Vec<usize>) -> usize {
     println!("{:?}", mid_point);
     vector[mid_point]
 }
+
 // and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+
+use std::collections::HashMap;
+
+fn mode(vector: &Vec<usize>) -> usize {
+    let mut map = HashMap::new();
+    for num in vector {
+        let count = map.entry(num).or_insert(0);
+        *count += 1;
+    }
+
+    let mut count_vec: Vec<_> = map.into_iter().collect();
+    count_vec.sort_by(|a, b| b.1.cmp(&a.1));
+
+    println!("{:?}", count_vec);
+
+    *count_vec[0].0
+}
