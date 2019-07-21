446fe04b84b7bed1f799e08134a470ca2f053d1e
Christian Bell <christiantcbell@gmail.com>
2019-07-21 16:05:18 +0100

    added median function

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ fn main() {
     let mean = mean(&v);
     println!("The mean is: {}", mean);
 
-
+    let mut med_vec = vec![45, 43, 88, 102, 55, 1, 2];
+    let median = median(&mut med_vec);
+    println!("The median is: {}", median);
 }
 
+// Given a list of integers, use a vector and return the mean (the average value),
 fn mean(vector: &Vec<usize>) -> usize {
     let mut counter = 0;
     for i in vector {
@@ -16,6 +19,14 @@ fn mean(vector: &Vec<usize>) -> usize {
 
     &counter / vector.len()
 }
-// Given a list of integers, use a vector and return the mean (the average value),
+
 // median (when sorted, the value in the middle position),
+fn median(vector: &mut Vec<usize>) -> usize {
+    vector.sort();
+    println!("{:?}", vector);
+
+    let mid_point = vector.len() / 2;
+    println!("{:?}", mid_point);
+    vector[mid_point]
+}
 // and mode (the value that occurs most often; a hash map will be helpful here) of the list.
